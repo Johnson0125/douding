462b60e448b36779af9b135f0399775999633418
Johnson <zs.it@hotmial.com>
2023-05-25 23:19:42 +0800

    generate from start.spring.io
    (cherry picked from commit 39eaa55d678345ce1b87eb2442b7d3590141e275)

---
+++ src/main/java/com/douding/douding/DoudingApplication.java
@@ -0,0 +1,13 @@
+package com.douding.douding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoudingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douding/douding/DoudingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douding.douding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoudingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
