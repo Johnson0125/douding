025e3e5736afa69fd090587a42ba2e1fcc99ec3f
Johnson <zs.it@hotmail.com>
2024-03-07 23:15:28 +0800

    feat:
    password encoder and permit for login under security

--- src/main/java/com/doubao/douding/system/config/SecurityConfig.java
+++ src/main/java/com/doubao/douding/system/config/SecurityConfig.java
@@ -15,12 +15,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -35,6 +38,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @description Security config
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${jwt.public.key}")
@@ -48,8 +52,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                    .csrf(csrf -> csrf.ignoringRequestMatchers("/token"))
+        httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login","other").permitAll()
+                        .anyRequest().authenticated())
+                    .csrf(csrf -> csrf.ignoringRequestMatchers("/login"))
                     .httpBasic(Customizer.withDefaults())
                     .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -85,5 +91,14 @@ public class SecurityConfig {
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
         JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
+    }
+
+    /**
+     * password encoder
+     * @return password encoder
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
