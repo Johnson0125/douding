704a5a97a2dd7435af03030085e804b58ed89f22
Johnson <zs.it@hotmail.com>
2024-04-08 23:21:29 +0800

    feat: integration with redis

---
+++ src/main/java/com/doubao/douding/system/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.doubao.douding.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author Johnson
+ * @Date 2024/4/8-22:40
+ * @Description: redis config
+ **/
+@Configuration
+public class RedisConfig {
+
+    @Bean("redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(factory);
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        return stringRedisTemplate;
+    }
+}
